3663db2918b12a21f99108d19318d09c3fc176c8
Alejandro Gomez <alejandro@strike.me>
2023-04-16 00:02:36 +0200

    fix: typo

--- src/pages/relay/[url].tsx
+++ src/pages/relay/[url].tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Link from "next/Link";
+import Link from "next/link";
 import dynamic from "next/dynamic";
 import { Flex, Heading, Text } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
